cac355986ff77359a30a2d2b77b31ed71461381e
Rodrigocs <rod.cervs@gmail.comgit>
2019-05-16 01:12:11 -0500

    Remove basename

--- src/App.js
+++ src/App.js
@@ -101,7 +101,7 @@ function App() {
           games,
         }}
       >
-        <Router basename={process.env.PUBLIC_URL}>
+        <Router>
           <Route path='/' exact component={SplashScreen} />
           <Route path='/login/' component={Login} />
           <Route path='/selection/' component={props => checkAuth(props, Selection)} />
